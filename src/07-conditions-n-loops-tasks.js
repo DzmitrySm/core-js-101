cc145b417b44131bffdf659b1c571a86b723be12
DzmitrySm <magicjoker@yandex.ru>
2022-12-04 21:42:23 +0300

    feat: add task doRectanglesOverlap

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -140,8 +140,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const downSpotCoord1 = rect1.top + rect1.height;
+  const downSpotCoord2 = rect1.left + rect1.width;
+  if (downSpotCoord1 > rect2.top && downSpotCoord2 > rect2.left) {
+    return true;
+  } return false;
 }
 
 
